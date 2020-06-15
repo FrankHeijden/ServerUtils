c3d798c1b641f46d9aac30440b2175e1b8a792f0
Frank van der Heijden <frank.boekanier@gmail.com>
2020-06-15 13:15:00 +0200

    Disable update check onjoin if disabled

--- src/main/java/net/frankheijden/serverutils/listeners/MainListener.java
+++ src/main/java/net/frankheijden/serverutils/listeners/MainListener.java
@@ -1,5 +1,6 @@
 package net.frankheijden.serverutils.listeners;
 
+import net.frankheijden.serverutils.config.Config;
 import net.frankheijden.serverutils.tasks.UpdateCheckerTask;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
@@ -7,8 +8,12 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class MainListener implements Listener {
 
+    private static final Config config = Config.getInstance();
+
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerJoin(PlayerJoinEvent event) {
+        if (!config.getBoolean("settings.check-updates")) return;
+
         Player player = event.getPlayer();
         if (player.hasPermission("serverutils.notification.update")) {
             UpdateCheckerTask.start(player);
