b8b28bcdc0dcf4225251370587961651a70497a6
Frank van der Heijden <frank.boekanier@gmail.com>
2021-11-25 23:54:54 +0100

    v3.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 group = "net.frankheijden.serverutils"
 val dependencyDir = "${group}.dependencies"
-version = "3.1.3-SNAPSHOT"
+version = "3.2.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
