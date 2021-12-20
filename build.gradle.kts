f36ad53d92db562c39f3b2c82965e446f9f03d78
Frank van der Heijden <frank.boekanier@gmail.com>
2021-12-20 20:10:11 +0100

    Back to snapshots

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 group = "net.frankheijden.serverutils"
 val dependencyDir = "${group}.dependencies"
-version = "3.4.0"
+version = "3.4.1-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
