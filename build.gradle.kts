c95aa495601a85b4e126a3c48f812a3229df8cdc
Frank van der Heijden <frank.boekanier@gmail.com>
2022-03-02 14:47:51 +0100

    Bump cloud to 1.6.2 and commodore to 1.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ subprojects {
         implementation("cloud.commandframework:cloud-brigadier:${VersionConstants.cloudVersion}")
         implementation("com.github.FrankHeijden:MinecraftReflection:1.0.0")
         implementation("com.google.code.gson:gson:2.8.6")
-        implementation("me.lucko:commodore:1.11")
+        implementation("me.lucko:commodore:1.13")
         compileOnly("com.mojang:brigadier:1.0.18")
 
         testImplementation("org.assertj:assertj-core:3.18.1")
--- buildSrc/src/main/kotlin/VersionConstants.kt
+++ buildSrc/src/main/kotlin/VersionConstants.kt
@@ -1,5 +1,5 @@
 object VersionConstants {
-    const val cloudVersion = "1.6.1"
+    const val cloudVersion = "1.6.2"
     const val adventureVersion = "4.9.3"
     const val adventurePlatformVersion = "4.0.1"
     const val adventureMinimessageVersion = "4.2.0-SNAPSHOT"
