25e2dc20b95517119d50cef5872900209f0ba68d
Frank van der Heijden <frank.boekanier@gmail.com>
2022-06-10 13:19:51 +0200

    Initial update for 1.19

--- buildSrc/src/main/kotlin/VersionConstants.kt
+++ buildSrc/src/main/kotlin/VersionConstants.kt
@@ -1,7 +1,7 @@
 object VersionConstants {
-    const val cloudVersion = "1.6.2"
+    const val cloudVersion = "1.7.0-SNAPSHOT"
     const val adventureVersion = "4.9.3"
     const val adventurePlatformVersion = "4.0.1"
     const val adventureMinimessageVersion = "4.2.0-SNAPSHOT"
-    const val bstatsVersion = "2.2.1"
+    const val bstatsVersion = "3.0.0"
 }
