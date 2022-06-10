bcf7a5ee1f957dd43e00a2f77a00de1cc54f99bc
Frank van der Heijden <frank.boekanier@gmail.com>
2022-06-10 13:21:32 +0200

    Bump version to v3.5.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 group = "net.frankheijden.serverutils"
 val dependencyDir = "${group}.dependencies"
-version = "3.4.5-SNAPSHOT"
+version = "3.5.0-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
