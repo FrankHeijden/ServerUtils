9a779eed79bd2317b06f35ae0c7b6d284d72a089
multirp <multirp@gmail.com>
2022-12-29 16:27:07 -0500

    1.19.3 Support; Updated cloudCommand to 1.8.0 (#63)
    * Updated to MC1.19.3; updated cloudCommand to 1.8.0
    * Delete Bukkit/bin/main directory
    * Delete Bungee/bin/main directory
    * Delete Velocity/bin directory
    * Delete Common/bin/main directory

--- buildSrc/src/main/kotlin/VersionConstants.kt
+++ buildSrc/src/main/kotlin/VersionConstants.kt
@@ -1,5 +1,5 @@
 object VersionConstants {
-    const val cloudVersion = "1.8.0-SNAPSHOT"
+    const val cloudVersion = "1.8.0"
     const val adventureVersion = "4.11.0"
     const val adventurePlatformVersion = "4.1.2"
     const val adventureMinimessageVersion = "4.2.0-SNAPSHOT"
