dfec660fc751ade57aefb10c012d08b082486eca
Frank van der Heijden <frank.boekanier@gmail.com>
2022-12-29 13:35:07 -0800

    start 3.5.5-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 group = "net.frankheijden.serverutils"
 val dependencyDir = "${group}.dependencies"
-version = "3.5.4"
+version = "3.5.5-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
